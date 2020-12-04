7f5ff3cd10d01687db0d1490bd042a1f83436b60
Katie Wolf <katielaurenwolf@gmail.com>
2020-12-04 00:41:48 -0500

    Add files via upload
    new viz

---
+++ plain_python_code/tests_per_confirmed_case_by_country.py
@@ -0,0 +1,58 @@
+# #!/usr/bin/env python3
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd 
+import re
+import warnings
+warnings.filterwarnings("ignore")
+
+
+## Reading in Data ##
+df= pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+
+
+def get_testing_info(country_name):
+	country_info = df.loc[df['location'] == country_name]
+	cleaned_data = country_info[["date", "tests_per_case"]].copy()
+	cleaned_data = cleaned_data.dropna()
+
+	dates = cleaned_data["date"].tolist()
+	tests_per_case = cleaned_data["tests_per_case"].tolist()
+
+	print(dates)
+
+	return dates, tests_per_case
+
+
+def tests_per_case_plot(x, y, country_name):
+
+	months = []
+
+	for date in x:
+		month = re.findall(r"(2020-[0-9]{0,2}-01)", date)
+		if month:
+			months.append(month[0])
+
+
+	plt.figure(figsize = (12, 8))
+	plt.plot(x, y)
+	plt.title("{} Tests per Case". format(country_name), size = 25)
+	plt.xlabel("Date", size = 20)
+	plt.ylabel("Tests per Case", size = 20)
+	plt.yticks(size = 10)
+	plt.xticks(months, size = 10)
+	plt.show()
+
+
+
+def tests_per_case_visualization(country_name):
+	dates = get_testing_info(country_name)[0]
+	tests_per_case = get_testing_info(country_name)[1]
+
+	tests_per_case_plot(dates, tests_per_case, country_name)
+
+
+
+tests_per_case_visualization("Switzerland")
+

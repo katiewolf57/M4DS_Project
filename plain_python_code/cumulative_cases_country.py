538eb24166a22077f514a187888eef0277a29187
Katie Wolf <katielaurenwolf@gmail.com>
2020-12-10 19:51:17 -0500

    Update cumulative_cases_country.py
    updating code, improving readability

--- plain_python_code/cumulative_cases_country.py
+++ plain_python_code/cumulative_cases_country.py
@@ -1,19 +1,10 @@
 # #!/usr/bin/env python3
 
-import numpy as np 
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import pandas as pd 
 import re
 import warnings
 warnings.filterwarnings("ignore")
-
-## own stuff ##
-from main_functions import daily_increase
-from main_functions import moving_average
-from main_functions import get_country_info
-
-
 
 ## ~~ READING DATA ~~ ##
 confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
@@ -21,49 +12,43 @@ confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COV
 ## ~~ USEFUL VALUES FOR LATER ~~ ##
 cols = confirmed_df.keys()
 confirmed = confirmed_df.loc[:, cols[4]:cols[-1]]
-
 dates = confirmed.keys()
 
-## Creating List of "First Day of the Month" Dates for nicer plotting
-months = []
+def get_case_info(country_name):
+    country_cases = []
 
-for date in dates:
-	month = re.findall(r"([0-9]{1,2}/1/20)", date)
-	if month:
-		months.append(month[0])
+    for i in dates:
+        country_cases.append(confirmed_df[confirmed_df['Country/Region']==country_name][i].sum())
 
+    return country_cases
 
 ## ~~CUMULATIVE CASES OVER TIME~~ ##
-## window for moving average in days
-window = 7
 
-def cumulative_plot(x, y1, country):
+def cumulative_plot(x, y, country):
     # window is set as 14 in in the beginning of the notebook 
-    confirmed_avg = moving_average(y1, window)
-    
-    plt.figure(figsize=(12, 8))
-    plt.plot(x, y1)
-    plt.plot(x, confirmed_avg, color='red', linestyle='dashed')
-    plt.legend(['{} Confirmed Cases'.format(country), 'Moving Average {} Days'.format(window)], prop={'size': 20})
-    plt.title('{} Confirmed Cases'.format(country), size=30)
-    plt.xlabel('Date', size=20)
-    plt.ylabel('# of Cases', size=20)
-    plt.yticks(size=10)
 
-    plt.xticks(months, size=10)
+    months = []
+
+    for date in dates:
+        month = re.findall(r"([0-9]{1,2}/1/20)", date)
+        if month:
+            months.append(month[0])
+    
+    plt.figure(figsize = (15, 8))
+    plt.plot(x, y)
+    plt.title('{} Confirmed Cases'.format(country), size = 25)
+    plt.xlabel('Date', size = 20)
+    plt.ylabel('# of Cases', size = 20)
+    plt.yticks(size = 10)
+    plt.xticks(months, size = 10)
 
     plt.show()       
 
     
 def cumulative_visualization(country_name):
-    country_info = get_country_info(country_name)
-    country_cases = country_info[0]
+    country_cases = get_case_info(country_name)
     
     cumulative_plot(dates, country_cases, country_name)
 
 
 cumulative_visualization("Switzerland")
-
-
-
-
